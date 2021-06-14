9295c9c3cdda43ea5f620dfd09970a37e1af303d
Jian Rong Lee <leejianrong2@gmail.com>
2021-06-14 21:40:15 +0800

    removed settings icon from bottom nav bar, added it to profile page. Added touch and cards to profile.

---
+++ src/screens/Profile/RecycleScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+export default function RecycleScreen() {
+    return (
+      <Text>This is my recycling screen</Text>
+    );
+  }
